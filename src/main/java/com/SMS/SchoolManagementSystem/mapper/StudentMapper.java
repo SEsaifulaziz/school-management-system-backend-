db39ab5eab8e0a58c76b60fae14519ae8b0f8232
SaifulAziz <gamestopik712@gmail.com>
2026-04-04 01:04:04 -0700

    Add Mapper Class to have explicit mapping

---
+++ src/main/java/com/SMS/SchoolManagementSystem/mapper/StudentMapper.java
@@ -0,0 +1,32 @@
+package com.SMS.schoolmanagementsystem.mapper;
+
+import com.SMS.schoolmanagementsystem.dto.request.StudentRequestDto;
+import com.SMS.schoolmanagementsystem.dto.response.StudentResponseDto;
+import com.SMS.schoolmanagementsystem.entity.Student;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StudentMapper {
+
+    //DTO -> Entity
+    public Student toEntity(StudentRequestDto dto){
+        Student student = new Student();
+
+        student.setFirstName(dto.getFirstName());
+        student.setLastName(dto.getLastName());
+        student.setEmail(dto.getEmail());
+        student.setEnrolledGrade(dto.getEnrolledGrade());
+        return student;
+    }
+
+    //Entity -> ResponseDto
+    public StudentResponseDto toResponse(Student student){
+        StudentResponseDto response = new StudentResponseDto();
+
+        response.setId(student.getId());
+        response.setFullName(student.getFirstName() + " " + student.getLastName());
+        response.setEmail(student.getEmail());
+        response.setEnrolledGrade(student.getEnrolledGrade());
+        return response;
+    }
+}
