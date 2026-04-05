401c5fe346098b0da4c3fdbb01a6de3d055bcca9
SaifulAziz <gamestopik712@gmail.com>
2026-04-05 06:36:37 -0700

    Add SubjectMapper class

--- src/main/java/com/SMS/SchoolManagementSystem/mapper/SubjectMapper.java
+++ src/main/java/com/SMS/SchoolManagementSystem/mapper/SubjectMapper.java
@@ -1,16 +1,36 @@
 package com.SMS.schoolmanagementsystem.mapper;
 
 import com.SMS.schoolmanagementsystem.dto.request.SubjectRequestDto;
+import com.SMS.schoolmanagementsystem.dto.response.SubjectResponseDto;
 import com.SMS.schoolmanagementsystem.entity.Subject;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ClassMapper {
+public class SubjectMapper {
 
+    //DTO -> Entity
     public Subject toEntity(SubjectRequestDto dto){
         Subject subject = new Subject();
 
         subject.setName(dto.getName());
         subject.setDescription(dto.getDescription());
+        subject.setCode(dto.getCode());
+        subject.setGradeLevel(dto.getGradeLevel());
+        subject.setCreditHours(dto.getCreditHours());
+        return subject;
+    }
+
+    //Entity -> ResponseDto
+    public SubjectResponseDto toResponse(Subject subject){
+        SubjectResponseDto response = new SubjectResponseDto();
+
+        response.setId(subject.getId());
+        response.setName(subject.getName());
+        response.setDescription(subject.getDescription());
+        response.setCode(subject.getCode());
+        response.setGradeLevel(subject.getGradeLevel());
+        response.setCreditHours(subject.getCreditHours());
+        response.setActive(subject.getActive());
+        return response;
     }
 }
