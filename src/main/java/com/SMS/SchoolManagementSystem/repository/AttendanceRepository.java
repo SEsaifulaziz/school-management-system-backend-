690ee5e7178dc913586ab0207fc3582de3bfb5d4
SaifulAziz <gamestopik712@gmail.com>
2026-04-08 23:49:22 +0500

    Refactor add paging and custom quesries

--- src/main/java/com/SMS/SchoolManagementSystem/repository/AttendanceRepository.java
+++ src/main/java/com/SMS/SchoolManagementSystem/repository/AttendanceRepository.java
@@ -5,6 +5,7 @@ import com.SMS.schoolmanagementsystem.entity.Enrollment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -14,13 +15,17 @@ import java.util.List;
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
 
     Boolean existsByAttendanceDateAndEnrollment(LocalDate date, Enrollment enrollmentId);
-    List<Attendance> findAllByAttendanceDate(LocalDate date);
-    List<Attendance> findAttendanceByEnrollmentIn(List<Enrollment> enrollments);
+
+    Page<Attendance> findAllByAttendanceDate(LocalDate date, Pageable pageable);
+
+    Page<Attendance> findAttendanceByEnrollmentIn(List<Enrollment> enrollments, Pageable pageable);
+
+    @Query("SELECT COUNT(a) FROM Attendance a WHERE a.enrollment IN :enrollmets")
+    long countTotalClasses(List<Enrollment> enrollments);
+
+    @Query("SELECT COUNT(a) FROM Attendance a WHERE a.enrollment IN :enrollments AND a.status = 'PRESENT'")
+    long countPresentClasses(List<Enrollment> enrollments);
 
     Page<Attendance> findAll(Pageable pageable);
-
-//    long coundByEnrollmentStudentId(Long studentId);
-//    long countByEnrollmentStudentIdAndStatus(Long studentId, AttendenceEnum status);
-
 
 }
