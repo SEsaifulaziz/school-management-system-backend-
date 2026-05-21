a93d1a0734bf20bd58fa979447c89d873036e585
Saiful Aziz <gamestopik712@gmail.com>
2026-05-21 22:28:42 +0500

    feat: add api authorization

--- src/main/java/com/SMS/SchoolManagementSystem/config/WebSecurityConfig.java
+++ src/main/java/com/SMS/SchoolManagementSystem/config/WebSecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,9 +17,16 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/enrollements/**").hasAnyRole("STUDENT","TEACHER",)
+                        .requestMatchers("/api/v1/api/students/").permitAll()
+                        .requestMatchers("/admin/**").permitAll()
+                )
                 .formLogin(Customizer.withDefaults());
         return httpSecurity.build();
     }
+
+
 
 }
 
