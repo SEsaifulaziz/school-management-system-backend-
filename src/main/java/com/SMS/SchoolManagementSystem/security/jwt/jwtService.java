39b6b5069e51b96f0d6877a18c922739f1aa2e63
Saiful Aziz <gamestopik712@gmail.com>
2026-05-22 12:53:46 +0500

    created generate token method

--- src/main/java/com/SMS/SchoolManagementSystem/security/jwt/jwtService.java
+++ src/main/java/com/SMS/SchoolManagementSystem/security/jwt/jwtService.java
@@ -1,20 +1,34 @@
 package com.SMS.schoolmanagementsystem.security.jwt;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
 
 
 @Service
 public class jwtService {
 
+    //  Secret key (must be long enough)
     private static final String SECRET_KEY =
-            "mysecretkeymysecretkeymysecretkey123456";
+            "mysecretkeymysecretkeymysecretkey123456"; // at least 32 chars
+
 
     private Key getSigningKey(){
         return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
     }
 
+    // Generate Token
+    public String generateToken(String username){
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hour
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
 
 }
