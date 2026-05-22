63c39d154214b2803824cf3b848057499ba3d93c
Saiful Aziz <gamestopik712@gmail.com>
2026-05-22 12:49:23 +0500

    feat: created secrete key

--- src/main/java/com/SMS/SchoolManagementSystem/security/jwt/jwtService.java
+++ src/main/java/com/SMS/SchoolManagementSystem/security/jwt/jwtService.java
@@ -1,4 +1,20 @@
 package com.SMS.schoolmanagementsystem.security.jwt;
 
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+
+
+@Service
 public class jwtService {
+
+    private static final String SECRET_KEY =
+            "mysecretkeymysecretkeymysecretkey123456";
+
+    private Key getSigningKey(){
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+
 }
